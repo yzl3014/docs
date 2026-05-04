ac30667c8da83ce7cdbf10a5b51b8c78c450e660
圆周率3014 <79385954+yzl3014@users.noreply.github.com>
2026-05-04 17:15:38 +0800

    旧网址重定向

---
+++ site/redirect.js
@@ -0,0 +1,6 @@
+if (window.location.href.includes("/#/?id=trending_disclaimer")) {
+    location.replace("https://docs.yuanzj.top/terms/trending/");
+}
+if (window.location.href.includes("/#/?id=trending_activation")) {
+    location.replace("https://docs.yuanzj.top/activation/trending/");
+}
