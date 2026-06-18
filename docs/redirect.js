67cef3ee8074b439dc26d0b9c3ba17d689d4b68c
圆周率3014 <79385954+yzl3014@users.noreply.github.com>
2026-06-18 15:29:06 +0800

    添加终止支持通知

--- docs/redirect.js
+++ docs/redirect.js
@@ -2,5 +2,5 @@ if (window.location.href.includes("/#/?id=trending_disclaimer")) {
     location.replace("https://docs.yuanzj.top/terms/trending/");
 }
 if (window.location.href.includes("/#/?id=trending_activation")) {
-    location.replace("https://docs.yuanzj.top/activation/trending/");
+    location.replace("https://docs.yuanzj.top/support/trending/");
 }
